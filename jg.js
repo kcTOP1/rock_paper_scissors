620d826d282310a892f9473f2f5808b4e6326eb3
kcTOP1 <120762579+kcTOP1@users.noreply.github.com>
2023-02-25 19:48:13 -0500

    Added one round fucntion

--- jg.js
+++ jg.js
@@ -1,13 +1,47 @@
-function getComputerChoice() {
-    var compChoice = Math.floor(Math.random() *4);
-    if (compChoice == 1) {
-        console.log("Rock");
-    } else if (compChoice == 2) {
-        console.log("Paper");
-    } else if (compChoice == 3) {
-        console.log("Scissor");
-    }
-    return;
-}
-getComputerChoice();
 
+function getComputerChoice() {
+    let compChoice = Math.floor(Math.random() *3);
+    let compSel = "";
+    if (compChoice == 0) {
+        
+        compSel = "rock";
+    } else if (compChoice == 1) {
+        
+        compSel = "paper";
+    } else if (compChoice == 2) {
+        
+        compSel = "scissors";
+    }
+    return compSel;
+}
+
+
+
+function roundPlay(player, computer) {
+    if (player == computer) {
+        console.log("the same");
+    }
+    if (player == "rock" && computer == "paper") {
+        console.log("Player loses. Paper Wins");
+    } else if (player == "rock" && computer == "scissors") {
+        console.log("Player wins, rock beats scissors")
+    }
+    if (player == "paper" && computer == "rock") {
+        console.log("Player wins with paper");
+    } else if (player == "paper" && computer == "scissors") {
+        console.log("Player loses, scissors beat paper");
+    }
+    if (player == "scissors" && computer == "rock") {
+        console.log("player loses, rock beats scissors");
+    } else if (player == "scissors" && computer == "paper") {
+        console.log("Player wins, scissors beat paper");
+    }
+
+    
+}
+
+const playerSel = prompt("Enter rock, paper, or scissor: ");
+const computerSel = getComputerChoice();
+roundPlay(playerSel, computerSel);
+console.log("This is the player selection: " + playerSel);
+console.log("This is the computer selection: " + computerSel);
